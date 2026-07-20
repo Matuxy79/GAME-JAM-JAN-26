5f15c1be8c07d85f680ac91303e3ba38cb76e1ee
John Matukutire <124116361+Matuxy79@users.noreply.github.com>
2026-07-19 22:39:52 -0600

    refactor: remove obsolete colony scene files and scripts

--- new-game-project/src/scripts/controller/scenes/ColonyGameRoot.gd
+++
@@ -1,9 +0,0 @@
-## ColonyGameRoot script. main root for colony phase gameplay loop.
-extends Node2D
-class_name ColonyGameRoot
-
-func _ready():
-	process_mode = Node.PROCESS_MODE_ALWAYS
-	print("ColonyGameRoot initialized")
-	EventBus.game_started.emit()
-	EventBus.colony_event_logged.emit("Colony simulation started.")
--- new-game-project/src/scripts/controller/scenes/ColonyWorld.gd
+++
@@ -1,8 +0,0 @@
-## ColonyWorld script. hosts colony entities and controller nodes.
-extends Node2D
-class_name ColonyWorld
-
-@onready var colonist_manager: ColonistManager = $ColonistManager
-
-func _ready():
-	print("ColonyWorld initialized with ", colonist_manager.test_colonist_count, " test colonists")
